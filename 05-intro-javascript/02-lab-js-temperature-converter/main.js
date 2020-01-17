0a443cfc6345aeafd459add8a83203930d1c622e
rcsima7 <rcsima7@gmail.com>
2020-01-17 17:21:14 +0100

    Comment explaining expression approach

--- 05-intro-javascript/02-lab-js-temperature-converter/main.js
+++ 05-intro-javascript/02-lab-js-temperature-converter/main.js
@@ -1,6 +1,9 @@
 const userInput = prompt('Temperature in Fahrenheit you want to convert to Celsius');
 
 // implement code here
+/* Not sure that numeric expressions can take paretheses and abide
+by calculus rules. So separating expression in 2 parts. Making sure
+-32 happens first*/
 const firstValue = userInput - 32;
 const output = firstValue / 1.8;
 
