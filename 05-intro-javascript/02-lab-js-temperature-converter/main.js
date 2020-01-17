061bfe138e181873eb0873a54b29e10db7879a60
rcsima7 <rcsima7@gmail.com>
2020-01-17 17:18:04 +0100

    1st try: separate numberic expression to 2 parts

--- 05-intro-javascript/02-lab-js-temperature-converter/main.js
+++ 05-intro-javascript/02-lab-js-temperature-converter/main.js
@@ -1,7 +1,8 @@
 const userInput = prompt('Temperature in Fahrenheit you want to convert to Celsius');
 
 // implement code here
-const output = undefined;
+const firstValue = userInput - 32;
+const output = firstValue / 1.8;
 
 // When you are ready to test your program, uncomment the line below
 // alert('Converted ' + userInput + ' Fahrenheit to Celsius: ' + output);
