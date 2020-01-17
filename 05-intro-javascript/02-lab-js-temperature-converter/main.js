b80a7a49744d7c1e71fadf92b3a16c1ab5476649
rcsima7 <rcsima7@gmail.com>
2020-01-17 17:31:58 +0100

    uncomment last line

--- 05-intro-javascript/02-lab-js-temperature-converter/main.js
+++ 05-intro-javascript/02-lab-js-temperature-converter/main.js
@@ -8,4 +8,4 @@ const firstValue = userInput - 32;
 const output = firstValue / 1.8;
 
 // When you are ready to test your program, uncomment the line below
-// alert('Converted ' + userInput + ' Fahrenheit to Celsius: ' + output);
+alert('Converted ' + userInput + ' Fahrenheit to Celsius: ' + output);
