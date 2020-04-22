78832ede6b03c450cc335a8b0ce5fafdbaede0a7
rcsima7 <rcsima7@gmail.com>
2020-04-22 18:24:16 +0200

    Lesson8 update

--- 08-javascript-http-requests/03-lab-ping-fb-api/main.js
+++ 08-javascript-http-requests/03-lab-ping-fb-api/main.js
@@ -19,9 +19,3 @@ document.addEventListener('DOMContentLoaded', function () {
   submit.addEventListener('click', handleSubmit);
 
 });
-
-// 6111918826961
-// EAAHeiXofApgBAIYh2Cl68GxMD7SE2mJMGgwZAzZCsH6BZAVoZBSCDrZCuzQufV9QvB7p4DbeElIqON1ya9qr9Ur8HcKqxs4ZBY8GZCHWCkBezhc7n8EScZBcjAu6DZAz03XKHs8D0aDjOrWD3IuFT6FlITt0adJIZAMUTZCeD3qKToa1ZCuRZApiihCGd9cAUMT6ysutiLhKu55ohZAu22tgAPYv1zcQFZBhEgQ99sZD
-// 4109a8b2f512e0c222655be33ff9f43fd66c482a2172dadd3db8e67b5be24bed
-
-// ,name,created_time,effective_status,objective,account_id,daily_budget
